74d8af63990462a833673051f3207415bf70958f
huanyu.li <cn.hylee@gmail.com>
2026-03-16 23:06:00 +0800

    feat: implement channel-based fibonacci generator

---
+++ fibonacci/fibonacci.go
@@ -0,0 +1,17 @@
+package main
+
+func fibonacci(n int) <-chan int {
+	ch := make(chan int)
+
+	go func() {
+		defer close(ch)
+
+		a, b := 0, 1
+		for range n {
+			ch <- a
+			a, b = b, a+b
+		}
+	}()
+
+	return ch
+}
